94d36cce10c752040107953903b3a002ee531c7c
Manna Soumya <sm323265@gmail.com>
2025-10-02 11:37:58 +0530

    Added commitId and build time in executable

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 set -e
-echo -n "Building"
+echo -n "Building "
+
 if test -e "vm-go"; then
   mv vm-go vm-go.old
 fi
-go build -ldflags "-s -w" vm-go.go
-rm vm-go.old
+
+go build -ldflags="-s -w -X main.gitCommit=$(git rev-parse HEAD) -X main.buildTime=$(date -u '+%Y-%m-%dT%H:%M:%SZ')"
+
+[ -f "vm-go.old" ] && rm "vm-go.old"
 chmod +x ./vm-go
+
 echo "...done"
 echo "Executable created. Run './vm-go'"
