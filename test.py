d7c8e2801f8415fa1a7f4e527d667f83cb5a1bb1
Manna Soumya <sm323265@gmail.com>
2026-02-25 23:15:29 +0530

    Added `quiet` mode in test.py

--- test.py
+++ test.py
@@ -30,56 +30,76 @@ def parse_arguments(arr, argument, bool=False, verbose=False):
         print(f"ERROR: Argument '{argument}' not found")
     raise Exception("ArgumentNotFound")
 
+def print_(message, end="\n", quiet=False):
+    if not quiet:
+        print(message, end=end)
+
 def usage(exit_code):
     print(f"Usage: python {sys.argv[0]} [OPTIONS]")
     print("\nOPTIONS:")
     print("   -i  (str)  : Run a specific test file in `examples` folder")
+    print("   -q  (bool) : Enable quiet mode")
     print("   -h  (bool) : Print this help and exit")
     print()
     print("If no arguments are passed, all tests are run")
     if exit_code != None:
         sys.exit(exit_code)
 
-
-def run_test(file_):
+def run_test(file_, quiet):
+    if not file_.endswith(".vasm"):
+        file_ = file_ + ".vasm"
     global error_count
-    dashes_count = len(file_)+FIXED_DASH_BLOCK_SIZE
-    print("-"*dashes_count)
-    print(f"Executing: {file_}")
+    dashes_count = len(file_) + FIXED_DASH_BLOCK_SIZE
+
+    # if not quiet:
+    print_("-" * dashes_count, quiet=quiet)
+    print_(f"Executing: {file_}", quiet=quiet)
+
     process        = subprocess.Popen(['./vm-go', '-i', f"examples/{file_}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     file_content   = open(f"./tests/expected/{file_.removesuffix('.vasm')}.expected").read()
+
     try:
-        print(f"Testing  : {file_}",end="")
+        if not quiet:
+            print_(f"Testing  : {file_}", end="", quiet=quiet)
         assert stdout.decode("utf-8") == file_content, "TestFail"
-        print("...Ok")
-        print("-"*dashes_count)
+
+        print_("...Ok", quiet=quiet)
+        print_("-" * dashes_count, quiet=quiet)
     except Exception as e:
-        print("\n")
-        print("stdout:")
-        print()
-        print(stdout.decode("utf-8"))
+        print_("\n", quiet=quiet)
+        print_("stdout:", quiet=quiet)
+        print_("", quiet=quiet)
+        print_(stdout.decode("utf-8"), quiet=quiet)
         error_count = error_count + 1
 
 if __name__ == "__main__":
+    quiet = False
     try:
         if parse_arguments(sys.argv, 'h', True):
             usage(0)
     except Exception as e:
         pass
+
     try:
-        test_file = parse_arguments(sys.argv,'i')
+        if parse_arguments(sys.argv, 'q', True):
+            quiet = True
+    except Exception as e:
+        pass
+
+    try:
+        test_file = parse_arguments(sys.argv, 'i')
     except Exception as e:
         pass
 
     if test_file:
-        run_test(test_file)
+        run_test(test_file, quiet)
         sys.exit(exit_code)
 
     for example_file in all_examples:
         if example_file in ignore_direct_execution:
             continue
-        run_test(example_file)
+        run_test(example_file, quiet)
 
     if error_count > 0:
         exit_code = 1
